1be1a324aa392937e02462c6c8ee822004e379e6
yuiosa <94675428+yuiosa@users.noreply.github.com>
2022-06-03 15:56:00 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "http://chva.ttkoyo.ga/sub?" }],
         remoteConfig: [
           {
             label: "universal",
