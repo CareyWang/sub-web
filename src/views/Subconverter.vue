136fedb1f856abb3f75a803950b2d0f5e768756e
OutisJ <55068362+OutisJ@users.noreply.github.com>
2023-01-31 08:23:33 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -296,8 +296,118 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://back.nobodysub.top" }],
         remoteConfig: [
+        {
+label: "ACL4SSR",
+options: [
+{
+label: "ACL4SSR_Online 默认版 分组比较全 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
+},
+{
+label: "ACL4SSR_Online_AdblockPlus 更多去广告 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_AdblockPlus.ini"
+},
+{
+label: "ACL4SSR_Online_NoAuto 无自动测速 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini"
+},
+{
+label: "ACL4SSR_Online_NoReject 无广告拦截规则 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoReject.ini"
+},
+{
+label: "ACL4SSR_Online_Mini 精简版 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini"
+},
+{
+label: "ACL4SSR_Online_Mini_AdblockPlus.ini 精简版 更多去广告 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_AdblockPlus.ini"
+},
+{
+label: "ACL4SSR_Online_Mini_NoAuto.ini 精简版 不带自动测速 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini"
+},
+{
+label: "ACL4SSR_Online_Mini_Fallback.ini 精简版 带故障转移 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_Fallback.ini"
+},
+{
+label: "ACL4SSR_Online_Mini_MultiMode.ini 精简版 自动测速、故障转移、负载均衡 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"
+},
+{
+label: "ACL4SSR_Online_Full 全分组 重度用户使用 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini"
+},
+{
+label: "ACL4SSR_Online_Full_NoAuto.ini 全分组 无自动测速 重度用户使用 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini"
+},
+{
+label: "ACL4SSR_Online_Full_AdblockPlus 全分组 重度用户使用 更多去广告 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini"
+},
+{
+label: "ACL4SSR_Online_Full_Netflix 全分组 重度用户使用 奈飞全量 (与Github同步)",
+value:
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Netflix.ini"
+},
+{
+label: "ACL4SSR 本地 默认版 分组比较全",
+value: "config/ACL4SSR.ini"
+},
+{
+label: "ACL4SSR_Mini 本地 精简版",
+value: "config/ACL4SSR_Mini.ini"
+},
+{
+label: "ACL4SSR_Mini_NoAuto.ini 本地 精简版+无自动测速",
+value: "config/ACL4SSR_Mini_NoAuto.ini"
+},
+{
+label: "ACL4SSR_Mini_Fallback.ini 本地 精简版+fallback",
+value: "config/ACL4SSR_Mini_Fallback.ini"
+},
+{
+label: "ACL4SSR_BackCN 本地 回国",
+value: "config/ACL4SSR_BackCN.ini"
+},
+{
+label: "ACL4SSR_NoApple 本地 无苹果分流",
+value: "config/ACL4SSR_NoApple.ini"
+},
+{
+label: "ACL4SSR_NoAuto 本地 无自动测速 ",
+value: "config/ACL4SSR_NoAuto.ini"
+},
+{
+label: "ACL4SSR_NoAuto_NoApple 本地 无自动测速&无苹果分流",
+value: "config/ACL4SSR_NoAuto_NoApple.ini"
+},
+{
+label: "ACL4SSR_NoMicrosoft 本地 无微软分流",
+value: "config/ACL4SSR_NoMicrosoft.ini"
+},
+{
+label: "ACL4SSR_WithGFW 本地 GFW列表",
+value: "config/ACL4SSR_WithGFW.ini"
+}
+]
+}, 
           {
             label: "universal",
             options: [
