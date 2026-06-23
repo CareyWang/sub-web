c3a686eca64945d483bb1834ccc340723528872f
CareyWong <careywong@pm.me>
2026-06-23 12:28:30 +0800

    chore(deps): 移除 vite-plugin-svg-icons，消除 postcss@5 漏洞链
    - GitHub 图标改为内联 SVG，移除雪碧图插件及 svg-baker/postcss@5 依赖
    - 修复 Dependabot security_update_not_possible 报错

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon/index.vue'// svg component
-import 'virtual:svg-icons-register'
 
 // register globally
 Vue.component('svg-icon', SvgIcon)
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 const srcDir = fileURLToPath(new URL('./src', import.meta.url))
-const svgDir = fileURLToPath(new URL('./src/icons/svg', import.meta.url))
 
 export default defineConfig({
   plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [svgDir],
-      symbolId: 'icon-[name]'
-    })
+    vue()
   ],
   resolve: {
     alias: {
